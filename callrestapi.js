0842719e985aeae02de63c8657e04e32061e5aef
Andryk Giovany <andrykgiovanytc@gmail.com>
2025-04-12 19:54:36 -0600

    botones antes de id

--- callrestapi.js
+++ callrestapi.js
@@ -1,5 +1,5 @@
-//var url = "http://localhost:8080/api/soda";
-var url = "https://mysql-sodaapi.onrender.com/api/soda";
+var url = "http://localhost:8080/api/soda";
+//var url = "https://mysql-sodaapi.onrender.com/api/soda";
 
 function postRefersco() {
 
@@ -37,6 +37,60 @@ function postRefersco() {
     });
 }
 
+function editarRefresco(id) {
+    $.getJSON(url + '/' + id, function (data) {
+        const refresco = data.refresco;
+
+        if (refresco) {
+            $('#nombre').val(refresco.nombre);
+            $('#marca').val(refresco.marca);
+            $('#tipo').val(refresco.tipo);
+            $('#sabor').val(refresco.sabor);
+            $('#tamano').val(refresco.tamano);
+            $('#precio').val(refresco.precio);
+            $('#stock').val(refresco.stock);
+
+            $('#btn-update').show().data('id', id);
+        } else {
+            alert('Refresco no encontrado.');
+        }
+    });
+}
+
+
+function updateRefresco() {
+    const id = $('#btn-update').data('id'); // ID almacenado
+
+    var mysoda = {
+        nombre: $('#nombre').val(),
+        marca: $('#marca').val(),
+        tipo: $('#tipo').val(),
+        sabor: $('#sabor').val(),
+        tamano: $('#tamano').val(),
+        precio: $('#precio').val(),
+        stock: $('#stock').val()
+    };
+
+    $.ajax({
+        url: url + '/' + id,
+        type: 'PUT',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify(mysoda),
+        success: function (data) {
+            alert('Refresco actualizado');
+            $('#btn-update').hide();
+            getRefrescos();
+        },
+        error: function (err) {
+            alert('Error al actualizar refresco');
+            console.error(err);
+        }
+    });
+}
+
+
+
 function getRefrescos() {
     console.log(url);
 
@@ -45,10 +99,10 @@ function getRefrescos() {
 
         var arrSodas = json.refrescos;
 
-        var htmlTableUsers = '<table border="1">';
+        var htmlTableUsers = '<table border="1">' +
+            '<tr><th>ID</th><th>Nombre</th><th>Marca</th><th>Tipo</th><th>Sabor</th><th>Tamaño</th><th>Precio</th><th>Stock</th><th>Acciones</th></tr>';
 
         arrSodas.forEach(function (item) {
-            console.log(item);
             htmlTableUsers += '<tr>' +
                 '<td>' + item.id + '</td>' +
                 '<td>' + item.nombre + '</td>' +
@@ -58,11 +112,32 @@ function getRefrescos() {
                 '<td>' + item.tamano + '</td>' +
                 '<td>' + item.precio + '</td>' +
                 '<td>' + item.stock + '</td>' +
-                '</tr>';
+                '<td>' +
+                '<button onclick="editarRefresco(' + item.id + ')">Editar</button> ' +
+                '<button onclick="deleteRefresco(' + item.id + ')">Eliminar</button>' +
+                '</td>'
+            '</tr>';
         });
 
         htmlTableUsers += '</table>';
 
         $('#resultado').html(htmlTableUsers);
     });
+}
+
+function deleteRefresco(id) {
+    if (!confirm("¿Estás seguro de eliminar el refresco " + id + "?")) return;
+
+    $.ajax({
+        url: url + '/' + id,
+        type: 'DELETE',
+        success: function (data) {
+            alert('Refresco eliminado con éxito: ' + JSON.stringify(data));
+            getRefrescos();
+        },
+        error: function (err) {
+            alert('Error al eliminar refresco');
+            console.error(err);
+        }
+    })
 }
